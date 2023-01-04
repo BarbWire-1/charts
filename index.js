bc535e610644e856c2963cbc12865e1d4aa4ece1
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2023-01-04 18:52:35 +0100

    defined props on BG and TODOs

--- index.js
+++ index.js
@@ -5,79 +5,57 @@
 
 
 window.onload = () => {
-class ChartTable {
-    constructor (obj) {
-        this.type = obj.type;
-        this.element = obj.element;
-        this.data = obj.data;
-        this.background = {
-            width: obj.background.width,
-            height: obj.background.height,
-            fill: obj.background.fill,
-        };
-        this.colors = obj.colors;
-        
-        //TODO decide if this is obsolete
-        this.count = 0;
-        this.id = `chartTable${this.count}`
-        
-        
-        // CREATEAND APPEND SVG CONTAINER AND COLORED RECT
-        this.svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-        this.element.appendChild(this.svg);
-        
-        this.svg.setAttribute('width', this.width);
-        this.svg.setAttribute('height', this.height);
-        this.background = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-        this.svg.appendChild(this.background);
-        
-        this.background.setAttribute('width', '100%');
-        this.background.setAttribute('height', '100%');
-        this.background.setAttribute('fill', '#e2e2e2');
-       
-        //TODO <g> element to hold the graphs
-        //TODO why is this.svg undefined without the use of the proxy????
-        // although I don't even get/set them here?
-        this.proxy = new Proxy(this, {
-            set: (target, property, value) => {
-                    target[ property ] = value;
-                    return true;
-                },
-                get: (target, property) => {
-                    return target[ property ];
-                },
-            });
     
-    }
-   
-    // get width() {
-    //     //console.log(`Getting width: ${this.background.width}`);
-    //     return this.background.width;
-    // }
-    // set width(nv) {
-    //     //console.log(`Setting width: ${nv}`);
-    //     this.background.width = nv
-    //     this.svg.setAttribute('width', nv)
-    // }
-    get height() {
-        //console.log(`Getting height: ${this.background.height}`);
-        return this.background.height;
-    } 
-    set height(nv) {
-        //console.log(`Setting height: ${nv}`);
-        this.background.height = nv
-        this.svg.setAttribute('height', nv)
-    }
-    get fill() {
-        //console.log(`Getting fill: ${this.background.fill}`);
-        return this.background.fill;
-    }
-    set fill(nv) {
-        //console.log(`Setting height: ${nv}`);
-        this.background.fill = nv
-        this.background.setAttribute('fill', nv)
-    }
-}
+    class ChartTable {
+        
+        constructor (obj) {
+            this.type = obj.type;
+            this.element = obj.element;
+            this.data = obj.data;
+            this.background = {
+                width: obj.background.width,
+                height: obj.background.height,
+                fill: obj.background.fill,
+            };
+            this.colors = obj.colors;
+            
+            //TODO decide if this is obsolete
+            this.count = 0;
+            this.id = `chartTable${this.count}`
+            
+            
+            // CREATE AND APPEND SVG CONTAINER AND COLORED RECT
+            // SAME FOR ALL TYPES - DON'T RE-RENDER FOR CHANGED CHART
+            this.svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+            this.element.appendChild(this.svg);
+            
+            this.svg.setAttribute('width', this.width);
+            this.svg.setAttribute('height', this.height);
+            this.background = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+            this.svg.appendChild(this.background);
+            
+            this.background.setAttribute('width', '100%');
+            this.background.setAttribute('height', '100%');
+            this.background.setAttribute('fill', '#e2e2e2');
+        
+            //TODO <g> element to hold the graphs
+            //TODO why is this.svg undefined without the use of the proxy????
+            // although I don't even get/set them here?
+            this.proxy = new Proxy(this, {
+                set: (target, property, value) => {
+                        target[ property ] = value;
+                        return true;
+                    },
+                    get: (target, property) => {
+                        return target[ property ];
+                    },
+                });
+        
+        }
+    
+    };
+    
+    // DEFINE STYLE-PROPS FOR HOLDING BACKGROUND
     Object.defineProperty(ChartTable.prototype, 'width', {
         get() { return this.background.width },
         set(newValue) {
@@ -85,11 +63,25 @@ class ChartTable {
             this.svg.setAttribute('width', newValue)
            
         }
-    })
+    });
+    
+    Object.defineProperty(ChartTable.prototype, 'height', {
+        get() { return this.background.height },
+        set(newValue) {
+            this.svg.setAttribute('height', newValue)
+        }
+    });
+    
+    Object.defineProperty(ChartTable.prototype, 'fill', {
+        get() { return this.background.fill },
+        set(newValue) {
+            this.background.setAttribute('fill', newValue)
+        }
+    });
     
     
     
- 
+    // USAGE
     let testElement = document.getElementById('chart-table')
     let testObject = new ChartTable(
         {
@@ -109,8 +101,9 @@ class ChartTable {
 
     testObject.fill = 'orange';
     testObject.width = 1000;
+    testObject.height = 500;
     testObject.data = [ 6, 7, 8, 9 ]
     testObject.type = 'bars'
     //TODO this way the background-el IS background. Not sure whether to keep this or not
-    // console.log(testObject.background.style.fill = 'green')
+    //console.log(testObject.background.style.fill = 'green')
 }
