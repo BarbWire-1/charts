4281d483bc8c787891431a2694a48c7a579c7420
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2023-01-05 13:23:27 +0100

    style obj not working :(

--- index.js
+++ index.js
@@ -9,16 +9,13 @@ window.onload = () => {
     class ChartTable {
         
         constructor (obj) {
+            
             this.type = obj.type;
             this.element = obj.element;
             this.data = obj.data;
-            this.background = {
-                // x: obj.background.x,
-                // y: obj.background.y,
-                width: obj.background.width,
-                height: obj.background.height,
-                fill: obj.background.fill,
-            };
+           
+            this.style = obj.style
+            
             this.colors = obj.colors;
             
             //TODO decide if this is obsolete
@@ -29,65 +26,99 @@ window.onload = () => {
             // CREATE AND APPEND SVG CONTAINER AND COLORED RECT
             // SAME FOR ALL TYPES - DON'T RE-RENDER FOR CHANGED CHART
             this.svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+            this.svg.setAttribute('width', this.style.width);
+            this.svg.setAttribute('height', this.style.height);
+            
+            this.bg = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+            this.bg.setAttribute('width', '100%');
+            this.bg.setAttribute('height', '100%');
+            this.bg.setAttribute('fill', this.style.fill);
+            
+             //TODO <g> element to hold the graphs
+        
+           
+            
+            this.svg.appendChild(this.bg);
             this.element.appendChild(this.svg);
-            
-            this.svg.setAttribute('width', this.width);
-            this.svg.setAttribute('height', this.height);
-            this.background = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-            this.svg.appendChild(this.background);
-            
-            this.background.setAttribute('width', '100%');
-            this.background.setAttribute('height', '100%');
-            this.background.setAttribute('fill', '#e2e2e2');
-            
-            // this.element.style.left = this.background.x +'px'
-        
-            //TODO <g> element to hold the graphs
-            //TODO why is this.svg undefined without the use of the proxy????
-            // although I don't even get/set them here?
-            this.proxy = new Proxy(this, {
-                set: (target, property, value) => {
-                        target[ property ] = value;
-                        return true;
-                    },
-                    get: (target, property) => {
-                        return target[ property ];
-                    },
-                });
-        
         }
+         
+    }
+
+
+
+        // DEFINE STYLE-PROPS FOR HOLDING BACKGROUND
+//         set style(newValue) {
+//             console.log('Setting style: ' + JSON.stringify(newValue));
+//             this._style = newValue;
+//             this.svg.setAttribute('width', this.style.width);
+//             this.svg.setAttribute('height', this.style.height);
+//             this.bg.setAttribute('fill', this.style.fill);
+//         }
+// 
+//         get style() {
+//             console.log('Getting style: ' + JSON.stringify(this._style));
+//             return this._style;
+//         }
+       
+   
     
-    };
     
-    // DEFINE STYLE-PROPS FOR HOLDING BACKGROUND
+   
+
+
+    
+    
+    
+   
+    
+    // WORKING BUT ON INSTANCE
     Object.defineProperty(ChartTable.prototype, 'width', {
-        get() { return this.background.width },
+        get() {
+            // console.log(this.style)//{x: 300, y: 300, width: 600, height: 600, fill: 'orange'}
+            console.log('Getting width: ' + this.style.width)
+            return this.style.width
+        },
         set(newValue) {
-            
+            console.log('Setting width: ' + newValue)
+            this.style.width = newValue
             this.svg.setAttribute('width', newValue)
            
         }
     });
     
     Object.defineProperty(ChartTable.prototype, 'height', {
-        get() { return this.background.height },
+        get() {
+            
+            console.log('Getting height: ' + this.style.height)
+            return this.style.height
+        },
         set(newValue) {
+            console.log('Setting height: ' + newValue)
             this.svg.setAttribute('height', newValue)
         }
     });
     
     Object.defineProperty(ChartTable.prototype, 'fill', {
-        get() { return this.background.fill },
+        get() {
+            console.log('Getting fill: ' + this.style.fill)
+            return this.style.fill
+        },
         set(newValue) {
-            this.background.setAttribute('fill', newValue)
+            this.bg.setAttribute('fill', newValue)
         }
     });
-    // Object.defineProperty(ChartTable.prototype, 'x', {
-    //     get() { return this.background.x },
-    //     set(newValue) {
-    //         this.element.style.left = newValue +'px'
-    //     }
-    // });
+    Object.defineProperty(ChartTable.prototype, 'x', {
+        get() { return this.style.x },
+        set(newValue) {
+            this.element.style.left = newValue +'px'
+        }
+    });
+    Object.defineProperty(ChartTable.prototype, 'y', {
+        get() { return this.style.y },
+        set(newValue) {
+            this.element.style.top = newValue + 'px'
+        }
+    });
     
     
     
@@ -98,24 +129,29 @@ window.onload = () => {
             type: 'pie',
             element: testElement,
             data: [ 1, 2, 3 ],
-            background: {x: 300, y: 300, width: 600, height: 600, fill: 'grey' },
+            style: {x: 300, y: 300, width: 600, height: 600, fill: 'grey' },
             colors: [ '#4a4e4d', '#0e9aa7', '#3da4ab', '#f6cd61', '#fe8a71' ]
         }
     )
 
     console.log(testObject)
     console.log(testObject);
+    //WHY??? not applied?
+    testObject.style.fill= 'orange'
 
     testObject.data = [ 5, 6, 7 ];
-    testObject.background.fill = 'green'// NOT APPLIED
-
-    testObject.fill = 'orange';
-    testObject.width = 600;
-    testObject.height = 500;
+    testObject.fill = 'blue';
+    testObject.style.width = 30;// not applied
+    testObject.width = 400;
+    testObject.height = 200;
     testObject.data = [ 6, 7, 8, 9 ]
     testObject.type = 'bars'
-    testElement.style.left = '300px'
-    testElement.style.top = '300px'
+    testObject.x = 100
+    testObject.y = 100
+    console.log(testObject.style)
+    console.log(testObject.style.width)// can read but not write here. check the structure!
+    console.log(testObject.fill)
+    // testElement.style.top = '300px'
     //TODO this way the background-el IS background. Not sure whether to keep this or not
     //console.log(testObject.background.style.fill = 'green')
 }
